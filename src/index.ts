cd9f2fe91bcf25688a64853552d79abc244abf20
Sergey Roytman <sergeyro@monday.com>
2023-07-12 13:49:17 +0300

    Export refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from "./components";
-export * from "./decorators/vertical-stories";
-export * from "./decorators/memory-stats";
-// export * from "./functions/createStoryMetaSettings";
-export * from "./functions/createComponentTemplate";
+export * from "./decorators";
+export * from "./functions";
