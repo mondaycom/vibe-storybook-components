9a022b170e624189e8b894c07194836684b67d7c
Sergey Roytman <sergeyro@monday.com>
2023-07-12 13:48:55 +0300

    DocFooter - add prop for decoupling

--- src/components/doc-footer/doc-footer.jsx
+++ src/components/doc-footer/doc-footer.jsx
@@ -5,16 +5,12 @@ import "./doc-footer.scss";
 const CSS_BASE_CLASS = "monday-storybook-footer";
 const bemHelper = BEMClass(CSS_BASE_CLASS);
 
-export const DocFooter = () => (
+export const DocFooter = ({ feedbackFormLink }) => (
   <div className={CSS_BASE_CLASS}>
     <SectionName>Feedback</SectionName>
     <div className={bemHelper({ element: "text" })}>
       Help us improve this pattern by providing feedback, asking questions, and leaving any other comments on
-      {/*TODO change link*/}
-      <a
-        href="https://forms.monday.com/forms/213ebddcb0d423ae5b6178fb6e8f7b3d?r=use1"
-        className={bemHelper({ element: "link" })}
-      >
+      <a href={feedbackFormLink} className={bemHelper({ element: "link" })}>
         this form
       </a>
     </div>
