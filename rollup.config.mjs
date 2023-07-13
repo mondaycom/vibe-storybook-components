da0b68e3803387b66663d35bb60a9f0cf2f35a80
Sergey Roytman <sergeyro@monday.com>
2023-07-13 10:12:59 +0300

    minor changes in rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,79 +1,77 @@
-import { DEFAULT_EXTENSIONS } from '@babel/core';
-import babel from '@rollup/plugin-babel';
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from '@rollup/plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import resolve from '@rollup/plugin-node-resolve';
-import url from '@rollup/plugin-url';
-import svgr from '@svgr/rollup';
-import { terser } from 'rollup-plugin-terser';
-import typescriptEngine from 'typescript';
-import pkg from './package.json' assert { type: 'json' };
+import babel from "@rollup/plugin-babel";
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import url from "@rollup/plugin-url";
+import svgr from "@svgr/rollup";
+import { terser } from "rollup-plugin-terser";
+import typescriptEngine from "typescript";
+import pkg from "./package.json" assert { type: "json" };
+
+const EXTENSIONS = [".js", ".jsx", ".ts", ".tsx"];
 
 const config = {
-  input: './src/index.ts',
+  input: "./src/index.ts",
   output: [
     {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: false,
-    },
-    {
       file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: false,
-    },
+      indent: false,
+      strict: false,
+      exports: "named",
+      format: "es"
+    }
   ],
   plugins: [
     postcss({
       plugins: [],
       minimize: true,
-      modules: true,
+      modules: true
     }),
     external({
-      includeDependencies: true,
+      includeDependencies: true
     }),
     typescript({
-      tsconfig: './tsconfig.json',
+      tsconfig: "./tsconfig.json",
       typescript: typescriptEngine,
-      include: ['*.js+(|x)', '**/*.js+(|x)', '*.ts+(|x)', '**/*.ts+(|x)'],
+      include: ["*.js+(|x)", "**/*.js+(|x)", "*.ts+(|x)", "**/*.ts+(|x)"],
       exclude: [
-        'coverage',
-        '.storybook',
-        'storybook-static',
-        'config',
-        'dist',
-        'node_modules/**',
-        '*.cjs',
-        '*.mjs',
-        '**/__snapshots__/*',
-        '**/__tests__',
-        '**/*.test.js+(|x)',
-        '**/*.test.ts+(|x)',
-        '**/*.mdx',
-        '**/*.story.jsx',
-        '**/*.story.tsx',
-        '**/*.stories.ts+(|x)',
-      ],
+        "coverage",
+        ".storybook",
+        "storybook-static",
+        "config",
+        "dist",
+        "node_modules/**",
+        "*.cjs",
+        "*.mjs",
+        "**/__snapshots__/*",
+        "**/__tests__",
+        "**/*.test.js+(|x)",
+        "**/*.test.ts+(|x)",
+        "**/*.mdx",
+        "**/*.story.jsx",
+        "**/*.story.tsx",
+        "**/*.stories.ts+(|x)"
+      ]
     }),
     babel({
-      extensions: [...DEFAULT_EXTENSIONS, '.ts', 'tsx'],
-      babelHelpers: 'runtime',
-      exclude: '**/node_modules/**',
+      extensions: EXTENSIONS,
+      babelHelpers: "bundled",
+      exclude: "**/node_modules/**"
     }),
 
     url(),
     svgr(),
-    resolve(),
+    nodeResolve({
+      extensions: [...EXTENSIONS, ".json", ".css"]
+    }),
     commonjs(),
-    terser(),
+    terser()
   ],
   watch: {
-    clearScreen: false,
-  },
+    clearScreen: false
+  }
 };
 
 export default config;
