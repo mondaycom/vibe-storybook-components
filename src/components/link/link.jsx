7c1ad0d0e40094e58112c33a130a677c1c67f660
Sergey Roytman <sergeyro@monday.com>
2023-07-13 12:24:58 +0300

    Fix Link component

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -2,39 +2,23 @@ import React from "react";
 import cx from "classnames";
 import PropTypes from "prop-types";
 import { BEMClass } from "../../helpers/utils/bem-helper";
+import CoreLink from "../../helpers/components/Link/Link";
 import "./link.scss";
-// import CoreLink from "../../../components/Link/Link";
 
 const BASE_CLASS = "monday-storybook-link";
 const bemHelper = BEMClass(BASE_CLASS);
 
-// TODO not working for some reason
-export const Link = ({ children, href, size, withoutSpacing, className }) => {
-  return (
-    <a
-      href={href}
-      className={cx(BASE_CLASS, className, {
-        [bemHelper({ state: "small" })]: size === Link.sizes.SMALL,
-        [bemHelper({ state: "medium" })]: size === Link.sizes.MEDIUM,
-        [bemHelper({ state: "with-spacing" })]: !withoutSpacing
-      })}
-    >
-      {children}
-    </a>
-  );
-};
-
-// export const Link = ({ children, href, size, withoutSpacing, className }) => (
-//   <CoreLink
-//     text={children}
-//     href={href}
-//     componentClassName={cx(BASE_CLASS, className, {
-//       [bemHelper({ state: "small" })]: size === Link.sizes.SMALL,
-//       [bemHelper({ state: "medium" })]: size === Link.sizes.MEDIUM,
-//       [bemHelper({ state: "with-spacing" })]: !withoutSpacing
-//     })}
-//   />
-// );
+export const Link = ({ children, href, size, withoutSpacing, className }) => (
+  <CoreLink
+    text={children}
+    href={href}
+    className={cx(BASE_CLASS, className, {
+      [bemHelper({ state: "small" })]: size === Link.sizes.SMALL,
+      [bemHelper({ state: "medium" })]: size === Link.sizes.MEDIUM,
+      [bemHelper({ state: "with-spacing" })]: !withoutSpacing
+    })}
+  />
+);
 
 Link.sizes = {
   SMALL: "small",
