9e8af2749dad4bcd223c075daba988674c54b9df
Sergey Roytman <sergeyro@monday.com>
2023-07-30 09:35:19 +0300

    Welcome and Change Log stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,20 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../storybook/**/*.stories.mdx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require('sass'),
+        },
+      },
+    },
   ],
   framework: {
     name: '@storybook/react-webpack5',
