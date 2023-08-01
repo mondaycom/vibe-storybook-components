e58aa1094f57ea81bd39174efb1c95826d058be9
Sergey Roytman <sergeyro@monday.com>
2023-08-01 11:21:19 +0300

    Remove @storybook/addon-interactions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     {
       name: '@storybook/addon-styling',
       options: {
