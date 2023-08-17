f8d7eb6e9f9355119fd563311d6efa0e266fe8fd
Sergey Roytman <sergeyro@monday.com>
2023-08-17 15:44:47 +0300

    Fix preview.tsx import

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -18,7 +18,7 @@ import {
 } from '../src';
 import { ComponentNameDecorator } from '../storybook/components';
 import { DocsContainer, DocsPage, Unstyled } from '@storybook/blocks';
-import '../src/tokens.scss';
+import '../src/_tokens.scss';
 
 const preview: Preview = {
   parameters: {
