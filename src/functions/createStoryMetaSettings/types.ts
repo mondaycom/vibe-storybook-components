b228274c5d1f57eca04bfa10737eb5fcbb07dbb2
Sergey Roytman <sergeyro@monday.com>
2023-10-22 19:14:34 +0300

    createStoryMetaSettings.ts - fix types

---
+++ src/functions/createStoryMetaSettings/types.ts
@@ -0,0 +1,40 @@
+import { ArgTypes, DecoratorFunction, StoryContext } from '@storybook/types';
+import { ReactRenderer } from '@storybook/react';
+
+export type EnumPropNames = {
+  propName: string;
+  enumName: string;
+};
+
+export type ActionProps = {
+  name: string;
+  linkedToPropValue: string;
+};
+
+export type IconMetaData = {
+  file: string;
+  name: string;
+};
+
+export type StoryMetaSettingsArgs = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  component: any;
+  enumPropNamesArray?: Array<string | EnumPropNames>;
+  iconPropNamesArray?: string[];
+  actionPropsArray?: Array<string | ActionProps>;
+  allIconsComponents: { [key: string]: unknown };
+  iconsMetaData?: IconMetaData[];
+  ignoreControlsPropNamesArray?: string[];
+};
+
+export type Decorator = DecoratorFunction<ReactRenderer> | ((Story: () => unknown, context: StoryContext) => Decorator);
+
+export type StoryMetaSettingsResult = {
+  argTypes: Partial<ArgTypes>;
+  decorators: Decorator[];
+};
+
+export type AllowedIcons = {
+  options: string[];
+  mapping: { [key: string]: unknown };
+};
