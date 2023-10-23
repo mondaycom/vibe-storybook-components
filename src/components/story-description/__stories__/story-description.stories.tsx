c5c458c60c4a49df016aa30a5eb98e6d147b7c99
Dhoni77 <aldrinmarz7@gmail.com>
2023-10-23 20:42:19 +0530

    feat: add story for story-description

---
+++ src/components/story-description/__stories__/story-description.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import StoryDescription from '../story-description';
+import StatusTag from '../../status-tag/status-tag';
+
+const meta: Meta<typeof StoryDescription> = {
+  component: StoryDescription,
+  title: 'Components/StoryDescription',
+};
+export default meta;
+
+type Story = StoryObj<typeof StoryDescription>;
+
+export const Overview: Story = {
+  args: {
+    description: 'Deprecated',
+    children: <StatusTag type="deprecated" />,
+    justify: 'Center',
+  },
+};
